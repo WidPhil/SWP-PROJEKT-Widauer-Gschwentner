02633f427edfe15d2324ec461721757ea6d68659
WidPhil <91456693+WidPhil@users.noreply.github.com>
2022-01-20 18:50:02 +0100

    Login Push

--- Veranstaltungssoftware/Loginform.Designer.cs
+++ Veranstaltungssoftware/Loginform.Designer.cs
@@ -32,26 +32,26 @@ namespace Veranstaltungssoftware
             this.btn_db = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_Title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_db
             // 
-            this.btn_db.Location = new System.Drawing.Point(117, 299);
+            this.btn_db.Location = new System.Drawing.Point(49, 353);
             this.btn_db.Name = "btn_db";
-            this.btn_db.Size = new System.Drawing.Size(182, 60);
+            this.btn_db.Size = new System.Drawing.Size(274, 47);
             this.btn_db.TabIndex = 0;
-            this.btn_db.Text = "open choosedb";
+            this.btn_db.Text = "Weiter";
             this.btn_db.UseVisualStyleBackColor = true;
             this.btn_db.Click += new System.EventHandler(this.btn_db_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(32, 25);
+            this.textBox1.Location = new System.Drawing.Point(142, 47);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(355, 20);
             this.textBox1.TabIndex = 1;
@@ -64,56 +64,56 @@ namespace Veranstaltungssoftware
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
             // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(407, 321);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(46, 17);
-            this.radioButton1.TabIndex = 3;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "YAY";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(407, 357);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(47, 17);
-            this.radioButton2.TabIndex = 4;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "NAY";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(32, 67);
+            this.comboBox1.Location = new System.Drawing.Point(142, 73);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(355, 21);
             this.comboBox1.TabIndex = 5;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(219, 100);
+            this.pictureBox1.Location = new System.Drawing.Point(331, 146);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(143, 152);
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
+            // lbl_Title
+            // 
+            this.lbl_Title.AutoSize = true;
+            this.lbl_Title.ForeColor = System.Drawing.Color.Chocolate;
+            this.lbl_Title.Location = new System.Drawing.Point(32, 13);
+            this.lbl_Title.Name = "lbl_Title";
+            this.lbl_Title.Size = new System.Drawing.Size(33, 13);
+            this.lbl_Title.TabIndex = 7;
+            this.lbl_Title.Text = "Login";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(32, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Server";
+            // 
             // Loginform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(509, 494);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(526, 405);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_Title);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btn_db);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Loginform";
             this.Text = "log in";
             this.Load += new System.EventHandler(this.Loginform_Load);
@@ -128,9 +128,9 @@ namespace Veranstaltungssoftware
         private System.Windows.Forms.Button btn_db;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_Title;
+        private System.Windows.Forms.Label label1;
     }
 }
