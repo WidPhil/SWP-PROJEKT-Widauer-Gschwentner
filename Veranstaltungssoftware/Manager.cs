62cbbc89eae5eb629e5ef31d2bec09fc289c5697
WidPhil <91456693+WidPhil@users.noreply.github.com>
2022-01-25 21:38:32 +0100

    Manager

--- Veranstaltungssoftware/Manager.cs
+++ Veranstaltungssoftware/Manager.cs
@@ -12,21 +12,19 @@ namespace Veranstaltungssoftware
     {
         public static string ConnectionString = "server = (localdb)\\MSSQLLocalDB;Database =" + NameofDB + ";Integrated Security = true";
 
-        public static List<string> dbList = new List<string>();
-        public static List<string> tList = new List<string>();
+    
         //public static List<string> colList = new List<string>(3);
 
         public static int iForDatabases;
         public static int iForTables;
 
-        //static string server = Login.server;
-        //static string database = chooseDatabase.database;
-        //static bool IntegratedSecurity = Loginform.IntegratedSecurity;
-
-        //static string Column1 = Table.col1;   //strings werden von choosetable übernommen
-        //static string Column2 = Table.col2;
-        //static string Column3 = Table.col3;
-        //static string Column4 = Table.col4;
+        public static string preferedbeverage = Veranstaltung.VorliebederGetränke;
+        public static string preferedfood = Veranstaltung.VorliebederSpeisen;
+        public static string street = Veranstaltung.Strasse;
+        public static string house = Veranstaltung.Hausnummer;
+        public static string postcode = Veranstaltung.PLZ;
+        public static string typeofEvent = Veranstaltung.TypeofEvent;
+       
 
         //public static Login connect = new Login();
         public static string NameofDB;
@@ -37,8 +35,7 @@ namespace Veranstaltungssoftware
 
 
         //cmd.ExecuteNonQuery();
-        //gibt Anzahl der zeilen
-        //sspi = mit windows credentials einloggen
+   
         //Datenbank erstellen, löschen und verändern
 
         public static void createDB()
@@ -109,8 +106,8 @@ namespace Veranstaltungssoftware
             {
                 con = new SqlConnection(ConnectionString);
                 con.Open();
-                //SqlCommand cmd = new SqlCommand("CREATE TABLE " + NameofTBL + " (" + "'" + Column1 + "' " + "INT PRIMARY KEY," + " '" + Column2 + "' " + "VARCHAR NOT NULL," + " '" + Column3 + "' " + "VARCHAR NOT NULL," + " '" + Column4 + "' " + "VARCHAR NOT NULL" + ");", con);
-                //cmd.ExecuteNonQuery();
+                SqlCommand cmd = new SqlCommand("CREATE TABLE " + NameofTBL + " (" + "'" + Column1 + "' " + "INT PRIMARY KEY," + " '" + Column2 + "' " + "VARCHAR NOT NULL," + " '" + Column3 + "' " + "VARCHAR NOT NULL," + " '" + Column4 + "' " + "VARCHAR NOT NULL" + ");", con);
+                cmd.ExecuteNonQuery();
                 iForTables++;
 
             }
@@ -145,8 +142,7 @@ namespace Veranstaltungssoftware
         {
             con = new SqlConnection(ConnectionString);
             con.Open();
-            //habe noch kein Gridview
-            //Gridview GV = new Gridview();
+          
             SqlCommand cmd = new SqlCommand();
             cmd.ExecuteNonQuery();
             con.Close();
